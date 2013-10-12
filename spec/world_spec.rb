fee8b1fb25370a44d43309473c99b3f8903af8d5
Gary Bernhardt <gary.bernhardt@gmail.com>
2013-10-11 17:02:22 -0700

    down spec passing

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -2,13 +2,14 @@
 load File.expand_path("../../selecta", __FILE__)
 
 describe World do
-  let("world") { World.blank(["one", "two", "three"]) }
+  let(:options) { Options.new(20) }
+  let(:world) { World.blank(options, ["one", "two", "three"]) }
 
   it "selects the first choice by default" do
     world.selected_choice.should == "one"
   end
 
-  xit "moves down the list" do
+  it "moves down the list" do
     world.down.selected_choice.should == "two"
   end
 end
