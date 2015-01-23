453ad280696bb9932fd3637bd364d5353b5849ed
Gary Bernhardt <gary.bernhardt@gmail.com>
2015-01-22 20:23:39 -0800

    simplify result sorting

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -34,11 +34,11 @@ describe "score" do
       expect(score("babababab", "aaaa").score).to be > 0
     end
 
-    it "scores 1, normalized to length, when the query equals the choice" do
+    it "scores the length of the match" do
       expect(score("a", "a").score).to eq 1.0
-      expect(score("ab", "ab").score).to eq 0.5
-      expect(score("a long string", "a long string").score).to eq 1.0 / "a long string".length
-      expect(score("spec/search_spec.rb", "sear").score).to eq 1.0 / "spec/search_spec.rb".length
+      expect(score("ab", "ab").score).to eq 2
+      expect(score("a long string", "a long string").score).to eq "a long string".length
+      expect(score("spec/search_spec.rb", "sear").score).to eq "sear".length
     end
   end
 
@@ -58,28 +58,25 @@ describe "score" do
   end
 
   describe "match quality" do
-    it "scores higher for better matches" do
-      expect(score("selecta.gemspec", "asp").score).to be > score("algorithm4_spec.rb", "asp").score
-      expect(score("README.md", "em").score).to be > score("benchmark.rb", "em").score
-      expect(score("search.rb", "sear").score).to be > score("spec/search_spec.rb", "sear").score
+    it "scores lower (better) for better matches" do
+      expect(score("selecta.gemspec", "asp").score).to be < score("algorithm4_spec.rb", "asp").score
+      expect(score("README.md", "em").score).to be < score("benchmark.rb", "em").score
     end
 
     it "scores shorter matches higher" do
-      expect(score("fbb", "fbb").score).to be > score("foo bar baz", "fbb").score
-      expect(score("foo", "foo").score).to be > score("longer foo", "foo").score
-      expect(score("foo", "foo").score).to be > score("foo longer", "foo").score
-      expect(score("1/2/3/4", "1/2/3").score).to be > score("1/9/2/3/4", "1/2/3").score
+      expect(score("fbb", "fbb").score).to be < score("foo bar baz", "fbb").score
+      expect(score("1x2x3x4", "1x2x3").score).to be < score("1x9x2x3x4", "1x2x3").score
     end
 
     it "sometimes scores longer strings higher if they have a better match" do
-      expect(score("long 12 long", "12").score).to be > score("1 long 2", "12").score
+      expect(score("long 12 long", "12").score).to be < score("1 long 2", "12").score
     end
 
     it "scores the tighter of two matches, regardless of order" do
       tight = "12"
       loose = "1padding2"
-      expect(score(tight + loose, "12").score).to eq 1.0 / (tight + loose).length
-      expect(score(loose + tight, "12").score).to eq 1.0 / (loose + tight).length
+      expect(score(tight + loose, "12").score).to eq "12".length
+      expect(score(loose + tight, "12").score).to eq "12".length
     end
   end
 end
