df0b8bb7152a1589540e447c24f5684ce675be03
Gavin Laking <gavinlaking@gmail.com>
2023-02-27 20:23:30 +0000

    Remove deprecated 'should' from specs and refactor for readability.

--- spec/text_spec.rb
+++ spec/text_spec.rb
@@ -1,12 +1,17 @@
 describe Text do
-  describe "truncating" do
-    it "can truncate itself to a given width" do
-      Text["this is long"].truncate_to_width(6).should == Text["this i"]
-    end
+  describe "truncates length" do
+    let(:text) { Text["this is long"] }
 
-    it "sometimes leaves empty strings behind when truncating" do
-      text = Text[:red, "one", :green, "two", :blue, "three"]
-      text.truncate_to_width(2).should == Text[:red, "on", :green, "", :blue, ""]
+    it "truncates itself to a given width" do
+      expect(text.truncate_to_width(6)).to eq(Text["this i"])
+    end
+  end
+
+  describe "when truncating" do
+    let(:text) { Text[:red, "one", :green, "two", :blue, "three"] }
+
+    it "leaves an empty string behind" do
+      expect(text.truncate_to_width(2)).to eq(Text[:red, "on", :green, "", :blue, ""])
     end
   end
 end
