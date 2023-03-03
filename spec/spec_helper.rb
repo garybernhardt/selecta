188d23a217603db571c627e7822f8ba6268ebf9b
Gary Bernhardt <gary.bernhardt@gmail.com>
2023-03-03 15:49:14 -0800

    fix incorrect code splitting in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 # We can't use `require` or `load` because of the Bash preamble on the script.
 source = File.read(File.expand_path("../../selecta", __FILE__))
-_preamble, source = source.split("#!ruby", 2)
+_preamble, source = source.split(/#!.*$/)
 eval(source)
